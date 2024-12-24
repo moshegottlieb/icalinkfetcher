5ce571245b97aae2b4351ba5f652b17e13980415
Moshe Gottlieb <moshe@sharkfood.com>
2024-12-24 14:27:43 +0100

    Added month name

--- src/today.ts
+++ src/today.ts
@@ -6,8 +6,8 @@ export class Today extends Canvas {
         let date_y = 0
         const now = new Date()
         const day = now.getDate()
-        const dow = now.toLocaleString('en-US', {  weekday: 'short' })
-        const date_font = this.font(415,'','')
+        const dow = now.toLocaleString('en-US', {  weekday: 'short' }).toUpperCase()
+        const date_font = this.font(400,'','')
         this.context.font = date_font
         let date_str = `${day}`
         let metrics = this.context.measureText(date_str)
@@ -23,5 +23,10 @@ export class Today extends Canvas {
         this.context.fillText(dow,(Canvas.WIDTH - metrics.width) / 2,dow_y + date_y + offset)
         this.context.font = date_font
         this.context.fillText(date_str,(Canvas.WIDTH - date_width) / 2,date_y + offset)
+        this.context.font = this.font(60,'',this.blackFamily)
+        const month = now.toLocaleString('en-US', {  month: 'long' }).toUpperCase()
+        metrics = this.context.measureText(month)
+        lineHeight = this.lineHeight(metrics)
+        this.context.fillText(month,(Canvas.WIDTH - metrics.width) / 2,Canvas.HEIGHT - Canvas.VSpace)
     }
 }
